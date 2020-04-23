77657c77cd2b355cda204013986b12ed465c24ef
avilde <vilde.andris@gmail.com>
2020-04-23 18:27:32 +0300

    signature colors

--- src/utils/commonUtils.ts
+++ src/utils/commonUtils.ts
@@ -10,6 +10,6 @@ export const mySignature = () => {
   console.info(
     `%c AV Mine Sweeper Game %c Author: Andris Vilde`,
     'background: #ff3b3b; color: #fff; font-size: 12px; border-radius: 3px 0 0 3px; font-family: Tahoma; padding: 0.25rem;',
-    'background: #bb7272; color: #000; font-size: 12px; border-radius: 0 3px 3px 0; font-family: Tahoma; padding: 0.25rem;'
+    'background: #fff; color: #000; font-size: 12px; border-radius: 0 3px 3px 0; font-family: Tahoma; padding: 0.25rem;'
   );
 };
