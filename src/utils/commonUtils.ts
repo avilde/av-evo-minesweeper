77c1d663141c6203933535645d764ae2cfa3329d
avilde <vilde.andris@gmail.com>
2020-04-24 10:41:57 +0300

    mock console

--- src/utils/commonUtils.ts
+++ src/utils/commonUtils.ts
@@ -17,7 +17,7 @@ export const getUiCharacter = (value: MapValue) => {
 };
 
 export const mySignature = () => {
-  console.info(
+  console.log(
     `%c AV Mine Sweeper Game %c Author: Andris Vilde`,
     'background: #ff3b3b; color: #fff; font-size: 0.75rem; border-radius: 0.25rem 0 0 0.25rem; font-family: Tahoma; padding: 0.25rem;',
     'background: #fff; color: #000; font-size: 0.75rem; border-radius: 0 0.25rem 0.25rem 0; font-family: Tahoma; padding: 0.25rem;'
